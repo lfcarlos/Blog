02f7a067b2fec4bcd8505c704ed7c819f1797227
leonardofigueiredo <carloslf1>
2023-03-15 21:11:49 -0300

    Criando Views e lista de detalhes

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,6 +4,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset()\
+                            .filter(status='puplished')
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -22,10 +28,15 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
+
+
 
