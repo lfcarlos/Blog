0a939a4a3b9cf2034c0afedd310d0bf996db5fe5
leonardofigueiredo <carloslf1>
2023-04-12 19:48:32 -0300

    Criação de views, templates, URLs e paginação de objetos

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+
 
 # Create your views here.
 
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # três postagens em cada página
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # Se a página não for um inteiro, exibe a primeira página
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Se a página estiver fora do intervalo, exibe a última página de resultados
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
-def post_detail(request,year,month,day,post):
+                  {'page': page, 'posts': posts})
+
+    # posts = Post.published.all()
+    # return render(request,
+    #               'blog/post/list.html',
+    #               {'posts': posts})
+
+
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
                              status='published',
                              publish__year=year,
@@ -17,3 +37,9 @@ def post_detail(request,year,month,day,post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('post_list/', views.post_list(), name='post_list')
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
